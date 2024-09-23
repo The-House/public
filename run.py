e88bf57b24771c3b1b4b279f6917d1c282bc91d7
Colton Chladek <colton.chladek@keway.com>
2024-09-23 16:40:11 -0500

    Rasa model

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 import os
-from rasa.__main__ import main
 
 if __name__ == '__main__':
-    # You can set any environment variables here
-    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Example for GPU handling (if applicable)
-
-    # Run Rasa server
-    main(["run", "--enable-api", "--cors", "*", "--debug"])
+    os.system("rasa run actions & rasa run --enable-api")
